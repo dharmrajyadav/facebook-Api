29f41434656a25b8c683940377267e9f1960c4c4
dharmrajyadav <dharmraj.yadav5@gmail.com>
2020-08-11 21:12:33 +0530

    url

--- fb-init.php
+++ fb-init.php
@@ -5,7 +5,7 @@
     require './vendor/autoload.php';
 
     $fb = new Facebook\Facebook([
-      'app_id' => '{}',
+      'app_id' => '',
       'app_secret' => '{}',
       'default_graph_version' => 'v2.4'
       ]);
