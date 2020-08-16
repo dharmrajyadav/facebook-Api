afa80e935febde2bcd1d7315622ca2e8845bbb94
Virtualbox_anmol <Virtualbox@gmail.com>
2020-08-16 17:48:28 +0530

    vendor directory untrack and other working

--- fb-init.php
+++ fb-init.php
@@ -5,13 +5,15 @@
     require './vendor/autoload.php';
 
     $fb = new Facebook\Facebook([
-      'app_id' => '',
-      'app_secret' => '',
+      'app_id' => '328073031701593',
+      'app_secret' => 'b0d8c8f2a5fd89c722128edb151e823c',
       'default_graph_version' => 'v2.4'
       ]);
     
     $helper = $fb->getRedirectLoginHelper();
-    $login_url = $helper->getLoginUrl("http://localhost/fb_login/");
-    // print_r($login_url);
-
+    //$login_url = $helper->getLoginUrl("http://localhost:8000/fb_login/");
+    $login_url = $helper->getLoginUrl("http://localhost:8000/fb-callback.php");
+    //$login_url = $helper->getLoginUrl("http://localhost:8000/");
+    //var_dump($login_url);
+	
 ?>
